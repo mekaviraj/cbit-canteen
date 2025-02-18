a9bc79483a76071daf64ba6900f53112453b7189
mekaviraj <mekaviraj1749@gmail.com>
2025-02-18 01:39:18 -0500

    2nd commmit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+// import React from "react";
+// import Login from "./components/Login";
+// import "/src/styles/Login.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <Login />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import CategoryPage from "./components/CategoryPage";
+import Login from "./components/Login";
+import "/src/styles/Login.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <CategoryPage />
+        <Login />
+      </div>
+    </>
+  );
+}
+
+export default App;
